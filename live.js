79fcdaa5df9805a3579530b869f80016b501f730
nohabrafi <charliebbarkin@tutanota.com>
2022-03-16 18:09:05 +0100

    just a little 'facelift'

--- live.js
+++ live.js
@@ -3,79 +3,81 @@ const Readline = require('@serialport/parser-readline');
 const fs = require('fs');
 
 var cnt = 0;
+var path = __dirname + '/dataFromSerial.json';
 
-var obj = {
-    table: []
-};
+if (!fs.existsSync(path)) {
 
-var port;
-
-var json = JSON.stringify(obj);
-
-fs.writeFile(__dirname + '/dataFromSerial.json', json, 'utf8', (err) => {
-    if (err) throw err;
-    console.log('The file has been created!');
-});
-
-try {
-
-    port = new SerialPort('COM3', {
-        baudRate: 9600
+    console.log("file does not exist. creating now...");
+    // create base obj 
+    var obj = {
+        table: []
+    };
+    // create json string to write to file
+    var json = JSON.stringify(obj);
+    // create file
+    fs.writeFile(path, json, 'utf8', (err) => {
+        if (err) throw err;
+        console.log('The file has been created!');
     });
-
-} catch (error) {
-
-    console.error(error);
-
 }
 
+// open new serialport
+var portName = 'COM3';
+var port;
+try {
+    port = new SerialPort(portName, {
+        baudRate: 9600
+    });
+} catch (error) {
+    console.error(error);
+}
+
+// Read the port data
 const parser = port.pipe(new Readline({
     delimiter: '\n'
-})); // Read the port data
-
+}));
+// if port is open
 port.on("open", () => {
     console.log('serial port open');
 });
-
+// if data is coming
 parser.on('data', dataFromSerial => {
 
+    // after 100 piece of data close connection
     if (cnt != 100) {
-
+        // current time 
         var curtime = new Date().toISOString();
-
+        // increase counter
         cnt++;
-
+        // console log info
         console.log("got this %d", dataFromSerial, "from arduino at", curtime);
         console.log("counter: ", cnt);
-
+        // read file contents
         fs.readFile(__dirname + '/dataFromSerial.json', 'utf8', function readFileCallback(err, dataFromFile) {
-
+            // if errors
             if (err) {
-
                 console.log(err);
-
             } else {
-
-                obj = JSON.parse(dataFromFile); //now it is an object
-
+                // data from file to obj
+                obj = JSON.parse(dataFromFile);
+                // add data to it
                 obj.table.push({
                     x: curtime,
                     y: dataFromSerial
-                }); //add some data
-
-                json = JSON.stringify(obj); //convert it back to json string
-                json = json.replace(/\\r/g, ''); // remove linebrake
-
+                });
+                //convert it back to json string
+                json = JSON.stringify(obj);
+                // remove linebrake
+                json = json.replace(/\\r/g, '');
+                // write it back to json file
                 fs.writeFile(__dirname + '/dataFromSerial.json', json, 'utf8', (err) => {
                     if (err) throw err;
                     console.log('Element has been added!');
-                }); // write it back to json file
+                });
             }
         });
-
     } else {
-
+        // close port after 100 pieces of data
         port.close();
-
     }
 });
